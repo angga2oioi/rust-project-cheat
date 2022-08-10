b4480f9a6201c8b1b9fb2fd6c07cb4c2113e0f45
Rizka Sanggar Darmawan <angga2oioi@gmail.com>
2022-08-11 03:56:34 +0700

    split files

---
+++ warp-webserver/src/controller/store_controller.rs
@@ -0,0 +1,62 @@
+use warp::{http, Filter};
+use std::collections::HashMap;
+use crate::model::struct_model::{
+    Store,
+    Item,
+    Id
+};
+
+
+pub async fn update_grocery_list(
+    item: Item,
+    store: Store
+    ) -> Result<impl warp::Reply, warp::Rejection> {
+        store.grocery_list.write().insert(item.name, item.quantity);
+
+
+        Ok(warp::reply::with_status(
+            "Added items to the grocery list",
+            http::StatusCode::CREATED,
+        ))
+}
+
+pub async fn delete_grocery_list_item(
+    id: Id,
+    store: Store
+    ) -> Result<impl warp::Reply, warp::Rejection> {
+        store.grocery_list.write().remove(&id.name);
+
+
+        Ok(warp::reply::with_status(
+            "Removed item from grocery list",
+            http::StatusCode::OK,
+        ))
+}
+
+pub async fn get_grocery_list(
+    store: Store
+    ) -> Result<impl warp::Reply, warp::Rejection> {
+        let mut result = HashMap::new();
+        let r = store.grocery_list.read();
+
+
+        for (key,value) in r.iter() {
+            result.insert(key, value);
+        }
+
+        Ok(warp::reply::json(
+            &result
+        ))
+}
+
+pub fn delete_json() -> impl Filter<Extract = (Id,), Error = warp::Rejection> + Clone {
+    // When accepting a body, we want a JSON body
+    // (and to reject huge payloads)...
+    warp::body::content_length_limit(1024 * 16).and(warp::body::json())
+}
+
+pub fn post_json() -> impl Filter<Extract = (Item,), Error = warp::Rejection> + Clone {
+    // When accepting a body, we want a JSON body
+    // (and to reject huge payloads)...
+    warp::body::content_length_limit(1024 * 16).and(warp::body::json())
+}
